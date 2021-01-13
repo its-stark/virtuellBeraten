5a11e55acc97212e32b4956c53e11529f6afdd11
Thomas Stark <stark@s3-medien.de>
2021-01-13 02:22:45 +0100

    book room and display link

--- virtuell-beraten.php
+++ virtuell-beraten.php
@@ -9,7 +9,148 @@ Author: tstark
 Author URI: https://www.s3-medien.de
 */
 
-function mysite_woocommerce_order_status_completed( $order_id ) {
-    error_log( "Order complete for order $order_id", 0 );
+/**
+ * @param $order_id
+ */
+//s3m_woocommerce_order_status_completed(149);
+//s3m_createBooking(149);
+function s3m_woocommerce_order_status_completed( $order_id ) {
+    error_log( "Order processing for order $order_id on site ", 0 );
+    $data = $order_id.get_site_url();
+    $booking_token = hash('sha512', $data, false);
+
+    $endpoint = 'http://videoapi.s3-medien.de/api/room/book';
+
+    $args = array(
+        'post_parent' => $order_id,
+        'post_type' => 'wc_booking',
+        'post_status' => 'any'
+    );
+
+    $bookings = get_posts($args);
+
+    //var_dump($bookings);
+    //error_log('bookings: '.count($bookings));
+
+    if(count($bookings) > 0){
+        s3m_createBooking($bookings[0]->ID);
+    }
 }
-add_action( 'woocommerce_order_status_completed', 'mysite_woocommerce_order_status_completed', 10, 1 );
+
+function s3m_createBooking($order_id){
+    $data = $order_id.get_site_url();
+    $booking_token = hash('sha512', $data, false);
+
+    $endpoint = 'http://videoapi.s3-medien.de/api/room/book';
+
+    $booking_meta = get_post_meta($order_id);
+
+    //20210121110000
+    $start_date = DateTime::createFromFormat('YmdHis', $booking_meta['_booking_start'][0]);
+    $end_date = DateTime::createFromFormat('YmdHis', $booking_meta['_booking_end'][0]);
+
+    $diff = $start_date->diff($end_date);
+    $duration = $diff->days * 24 * 60;
+    $duration += $diff->h * 60;
+    $duration += $diff->i;
+
+    $body = array(
+        'booking_token' => $booking_token,
+        'start_date' => $start_date->format('Y-m-d H:i'),
+        'duration' => $duration,
+        'max_participants' => '10'
+    );
+
+    //$body = wp_json_encode( $body );
+
+    $options = [
+        'body'        => $body,
+        'headers'     => [
+            'Authorization' => 'Bearer F43MOUs3Tah01DpGQqRGWQXLSNTAYHmqtj84Ti9z',
+            //'Content-Type' => 'application/json',
+        ],
+        'timeout'     => 60,
+        'redirection' => 5,
+        'blocking'    => true,
+        //'httpversion' => '1.0',
+        //'sslverify'   => false,
+        'data_format' => 'body',
+    ];
+
+    $response = wp_remote_post( $endpoint, $options );
+
+    //var_dump($response['body']);
+
+    $data = json_decode($response['body']);
+
+    update_post_meta($order_id, '_video_url', $data->link);
+
+    //send mail to customer
+
+}
+
+//add_action( 'woocommerce_order_status_processing', 's3m_woocommerce_order_status_completed', 10, 1 );
+//add_action( 'woocommerce_order_status_completed', 's3m_woocommerce_order_status_completed', 10, 1 );
+add_action('woocommerce_booking_paid', 's3m_createBooking', 10, 1 );
+//add_action('woocommerce_before_thankyou', 's3m_woocommerce_order_status_completed');
+
+function s3m_createVideoConferenceRoom($order_id){
+    //error_log( "Order processing for order $order_id on site ".get_site_url(), 0 );
+    $data = $order_id.get_site_url();
+    $booking_token = hash('sha512', $data, false);
+
+    $endpoint = 'http://videoapi.s3-medien.de/api/room/create';
+
+    $args = array(
+        'post_parent' => $order_id,
+        'post_type' => 'wc_booking',
+        'post_status' => 'any',
+    );
+
+    $bookings = get_posts($args);
+
+    if(count($bookings) > 0) {
+
+        $booking_meta = get_post_meta($bookings[0]->ID);
+
+        //var_dump($booking_meta);
+
+        //20210121110000
+        $start_date = DateTime::createFromFormat('YmdHis', $booking_meta['_booking_start'][0]);
+        $end_date = DateTime::createFromFormat('YmdHis', $booking_meta['_booking_end'][0]);
+
+        $diff = $start_date->diff($end_date);
+        $duration = $diff->days * 24 * 60;
+        $duration += $diff->h * 60;
+        $duration += $diff->i;
+
+        $body = array(
+            'booking_token' => $booking_token,
+            'start_date' => $start_date->format('Y-m-d H:i'),
+            'duration' => $duration,
+            'max_participants' => '10'
+        );
+
+        //$body = wp_json_encode( $body );
+
+        $options = [
+            'body' => $body,
+            'headers' => [
+                'Authorization' => 'Bearer F43MOUs3Tah01DpGQqRGWQXLSNTAYHmqtj84Ti9z',
+                //'Content-Type' => 'application/json',
+            ],
+            'timeout' => 60,
+            'redirection' => 5,
+            'blocking' => true,
+            //'httpversion' => '1.0',
+            //'sslverify'   => false,
+            'data_format' => 'body',
+        ];
+
+        $response = wp_remote_post($endpoint, $options);
+        $data = json_decode($response['body']);
+
+        update_post_meta($order_id, '_video_url', $data->link);
+        update_post_meta($order_id, '_room_created', date("Y-m-d H:i:s"));
+    }
+}
